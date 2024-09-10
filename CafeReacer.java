7d0cb63e8a548193b826617b3dc3f85f7c298df3
javier isaac sandoval <127066524+javier6441112@users.noreply.github.com>
2024-09-09 20:10:00 -0600

    Add files via upload

---
+++ CafeReacer.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.abstractfactory;
+
+/**
+ *
+ * @author user
+ */
+public class CafeReacer implements Moto {
+
+    @Override
+    public void acelerar(String acelerar) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void frenar(String frenar) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void alumbrar(Boolean alumnbrar) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Moto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.abstractfactory;
+
+/**
+ *
+ * @author user
+ */
+public abstract interface Moto {
+    
+    public void acelerar (String acelerar);
+    
+    public void frenar (String frenar);
+    
+    public void alumbrar (Boolean alumnbrar);
+   
+}
